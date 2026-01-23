decf15e43487a052189de828ffc51ad931f7eb5c
kdayski <58617773+kdayski@users.noreply.github.com>
2026-01-23 16:25:40 +0500

    code structuring for bigger projects pt. 1

---
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -0,0 +1,34 @@
+import Sizes from "./Utils/Sizes"
+import Time from "./Utils/Time"
+
+export default class Experience {
+	constructor(canvas) {
+		// Global access
+		window.experience = this
+
+		// Options
+		this.canvas = canvas
+
+		// Setup
+		this.sizes = new Sizes()
+		this.time = new Time()
+
+		// Sizes resize event
+		this.sizes.on('resize', () => {
+			this.resize()
+		})
+
+		// Time tick event
+		this.time.on('tick', () => {
+			this.update()
+		})
+	}
+
+	resize() {
+
+	}
+
+	update() {
+
+	}
+}
