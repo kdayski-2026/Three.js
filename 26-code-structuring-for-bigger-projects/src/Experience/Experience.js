003c9f2c22ef30552dcc82450748192b785a202e
kdayski <npoqpu6@gmail.com>
2026-01-25 13:28:45 +0500

    code structuring for bigger projects pt. 3

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -6,6 +6,7 @@ import Renderer from './Renderer'
 import World from './World/World'
 import Resources from './Utils/Resources'
 import sources from './sources'
+import Debug from './Utils/Debug'
 
 let instance = null
 export default class Experience {
@@ -20,6 +21,7 @@ export default class Experience {
 		this.canvas = canvas
 
 		// Setup
+		this.debug = new Debug()
 		this.sizes = new Sizes()
 		this.time = new Time()
 		this.scene = new THREE.Scene()
@@ -46,6 +48,34 @@ export default class Experience {
 
 	update() {
 		this.camera.update()
+		this.world.update()
 		this.renderer.update()
+	}
+
+	destroy() {
+		this.sizes.off('resize')
+		this.time.off('tick')
+
+		window.removeEventListener('resize')
+
+		// Traverse the whole scene
+		this.scene.traverse((child) => {
+			if (child instanceof THREE.Mesh) {
+				child.geometry.dispose()
+				for (const key in child.material) {
+					const value = child.material[key]
+					if (value && typeof value.dispose === 'function') {
+						value.dispose()
+					}
+				}
+			}
+		})
+
+		this.camera.controls.dispose()
+		this.renderer.instance.dispose()
+
+		if (this.debug.active) {
+			this.debug.ui.destroy()
+		}
 	}
 }
